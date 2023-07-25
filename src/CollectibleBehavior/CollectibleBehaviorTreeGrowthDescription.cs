f5e6bdaeb2ed7df6629b83ccb1452da722997c42
Craluminum2413 <beware.14b@gmail.com>
2023-07-25 15:20:46 +0300

    added tree growth time for item

---
+++ src/CollectibleBehavior/CollectibleBehaviorTreeGrowthDescription.cs
@@ -0,0 +1,47 @@
+using System.Text;
+using Vintagestory.API.Common;
+using Vintagestory.API.Config;
+using Vintagestory.API.MathTools;
+using Vintagestory.GameContent;
+using static ExtraInfo.TextExtensions;
+
+namespace ExtraInfo
+{
+    public class CollectibleBehaviorTreeGrowthDescription : CollectibleBehavior
+    {
+        public CollectibleBehaviorTreeGrowthDescription(CollectibleObject collObj) : base(collObj)
+        {
+        }
+
+        public override void GetHeldItemInfo(ItemSlot inSlot, StringBuilder dsc, IWorldAccessor world, bool withDebugInfo)
+        {
+            base.GetHeldItemInfo(inSlot, dsc, world, withDebugInfo);
+            AddTreeGrowthInfoForItem(inSlot, dsc, world);
+        }
+
+        private static void AddTreeGrowthInfoForItem(ItemSlot inSlot, StringBuilder dsc, IWorldAccessor world)
+        {
+            NatFloat sproutDays = null;
+            NatFloat matureDays = null;
+
+            if (inSlot.Itemstack.Collectible is ItemTreeSeed item)
+            {
+                var block = world.GetBlock(AssetLocation.Create("sapling-" + item.Variant["type"] + "-free", item.Code.Domain));
+                if (block == null) return;
+
+                sproutDays = block.Attributes["sproutDays"].AsObject<NatFloat>();
+                matureDays = block.Attributes["matureDays"].AsObject<NatFloat>();
+            }
+            else if (inSlot.Itemstack.Collectible is BlockPlant block && !string.IsNullOrEmpty(block.EntityClass))
+            {
+                sproutDays = block.Attributes["sproutDays"].AsObject<NatFloat>();
+                matureDays = block.Attributes["matureDays"].AsObject<NatFloat>();
+            }
+
+            if (sproutDays == null && matureDays == null) return;
+
+            dsc.AppendLine(Lang.Get("Will sprout in about {0} days", GetMinMax(sproutDays)));
+            dsc.AppendLine(Lang.Get("Will mature in about {0} days", GetMinMax(matureDays)));
+        }
+    }
+}
--- src/Core.cs
+++ src/Core.cs
@@ -1,5 +1,6 @@
-using Vintagestory.API.Client;
 using Vintagestory.API.Common;
+using Vintagestory.API.Util;
+using Vintagestory.GameContent;
 
 [assembly: ModInfo("Extra Info")]
 
@@ -7,9 +8,22 @@ namespace ExtraInfo;
 
 public class Core : ModSystem
 {
-    public override void StartClientSide(ICoreClientAPI api)
+    public override void Start(ICoreAPI api)
     {
-        base.StartClientSide(api);
+        base.Start(api);
+        api.RegisterCollectibleBehaviorClass("ExtraInfo:TreeGrowthDescription", typeof(CollectibleBehaviorTreeGrowthDescription));
         api.World.Logger.Event("started 'Extra Info' mod");
+    }
+
+    public override void AssetsFinalize(ICoreAPI api)
+    {
+        if (api.Side != EnumAppSide.Client) return;
+
+        foreach (var obj in api.World.Collectibles)
+        {
+            if (obj is not (ItemTreeSeed or BlockPlant)) continue;
+
+            obj.CollectibleBehaviors = obj.CollectibleBehaviors.Append(new CollectibleBehaviorTreeGrowthDescription(obj));
+        }
     }
 }
