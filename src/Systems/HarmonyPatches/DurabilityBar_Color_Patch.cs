c520e96b4bc4191ba44b69c789c30883c6a762bb
Dana <beware.14b@gmail.com>
2024-08-05 12:21:30 +0300

    fix durability bug

--- src/Systems/HarmonyPatches/DurabilityBar_Color_Patch.cs
+++ src/Systems/HarmonyPatches/DurabilityBar_Color_Patch.cs
@@ -2,8 +2,9 @@ namespace ExtraInfo;
 
 public static class DurabilityBar_Color_Patch
 {
-    public static void Postfix(ref int __result, CollectibleObject __instance, ItemStack itemstack)
+    public static void Postfix(ref int __result, CollectibleObject __instance, ItemStack itemstack, ICoreAPI ___api)
     {
+        if (!___api.Side.IsClient()) return;
         switch (__instance)
         {
             case BlockLiquidContainerBase liquidContainerBase:
